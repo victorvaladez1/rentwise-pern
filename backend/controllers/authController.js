be827343dac0e11450c596fd099c11ba108ba2f3
Victor Valadez <vvaladez315@gmail.com>
2025-07-04 11:00:42 -0500

    Create login function in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { findUserByEmail, createUser } = require("../models/userModel");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 async function register(req, res) {
     const { email, password, role } = req.body;
@@ -20,4 +23,29 @@ async function register(req, res) {
     }
 }
 
-module.exports = { register };
+async function login(req, res) {
+    const {email, password} = req.body;
+
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+        if (!isMatch) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user_id, role: user_role }, JWT_SECRET, {
+            expiresIn: "1h",
+        });
+
+        res.json({ token });
+    } catch (err) {
+        console.error("Login error:", err);
+        res.status(500).json({ error: "Server error" });
+    }
+}
+
+module.exports = { register, login };
