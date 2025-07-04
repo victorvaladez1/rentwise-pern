b1b7c9106cef08b998380e29b7e5dbb5a4ce5edf
Victor Valadez <vvaladez315@gmail.com>
2025-07-04 10:43:31 -0500

    Add authRoutes to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
+
+const authRoutes = require("./routes/auth");
+
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/api/test", async (req, res) => {
     try {
