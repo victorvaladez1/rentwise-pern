3aed5df5d75d89babf6865e0acad4f50fb0d0a7a
Victor Valadez <vvaladez315@gmail.com>
2025-07-04 16:39:20 -0500

    Create /summary route in payments

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createPayment, getPayments } = require("../controllers/paymentController");
+const { createPayment, getPayments, getRentSummary } = require("../controllers/paymentController");
 const verifyToken = require("../middleware/authMiddleware");
 
 router.post("/", verifyToken, createPayment);
 router.get("/", verifyToken, getPayments);
+router.get("/summary", verifyToken, getRentSummary);
 
 module.exports = router;
