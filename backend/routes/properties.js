5fc3872510fbc058290c06e7fa413c34aa2abc21
Victor Valadez <vvaladez315@gmail.com>
2025-07-04 14:51:18 -0500

    Create routes for editProperty and removeProperty in properties

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getProperties, addProperty } = require("../controllers/propertyController");
+const { 
+    getProperties, 
+    addProperty,
+    editProperty,
+    removeProperty,
+} = require("../controllers/propertyController");
 const verifyToken = require("../middleware/authMiddleware");
 
 router.get("/", verifyToken, getProperties);
 router.post("/", verifyToken, addProperty);
+router.put("/:id", verifyToken, editProperty);
+router.delete("/:id", verifyToken, removeProperty);
 
 module.exports = router;
