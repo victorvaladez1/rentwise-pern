db2a389cfdd0699459d922ac40af98b2caab33bb
Victor Valadez <vvaladez315@gmail.com>
2025-07-06 18:57:54 -0500

    Include register page route in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import PropertiesPage from './pages/Properties';
 import TenantsPage from './pages/Tenants';
@@ -17,6 +18,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           
           <Route path="/dashboard" element={
             <ProtectedRoute>
