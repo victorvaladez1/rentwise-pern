8f4ac1c62de229797f759dd0a8a2d61b3c4f481d
Victor Valadez <vvaladez315@gmail.com>
2025-07-06 19:52:25 -0500

    Add Tenat role to the register page

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -54,7 +54,8 @@ const RegisterPage: React.FC = () => {
                     className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
                 >
                     <option value="manager">Manager</option>
-                    <option value="viewer">Viewer</option>
+                    <option value="landlord">Landlord</option>
+                    <option value="tenant">Tenant</option>
                 </select>
 
                 <button
